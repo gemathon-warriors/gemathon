e8cac5ed876c940266334f03733e4e71b689360f
Ashish Upadhyay <ashish.upadhyaye@gmail.com>
2014-02-21 23:10:52 -0500

    Day on past month middle date added

--- app/controllers/tareek_controller.rb
+++ app/controllers/tareek_controller.rb
@@ -18,4 +18,8 @@ class TareekController < ApplicationController
     @result = Tareek::Dates.day_middle_of_next_month_date(params[:year],params[:month])
   end
 
+  def day_on_past_month_middle_date
+    @result = Tareek::Dates.day_middle_of_past_month_date(params[:year],params[:month])
+  end
+
 end
